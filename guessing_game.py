ffd6de844ac364fe7876d6aa3962fed2878829a6
Won Yang <won@gatech.edu>
2021-05-24 11:50:22 -0500

    adds solution

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,30 @@
 class GuessingGame():
+    answer = 0
+    is_solved = False
+
+    def __init__(self, answer):
+        self.answer = answer
+
+    def solved(self):
+        return self.is_solved
+
+    def guess(self, user_guess):
+        if (user_guess == self.answer):
+            self.is_solved = True
+            return 'correct'
+        if (user_guess > self.answer):
+            return 'high'
+        else:
+            return 'low'
+
+
+game = GuessingGame(10)
+
+print(game.solved())   # => False
+
+print(game.guess(5))  # => 'low'
+print(game.guess(20))  # => 'high'
+print(game.solved())   # => False
+
+print(game.guess(10))  # => 'correct'
+print(game.solved())   # => True
