a7444b8b23bd8a71bdae9288a7e4e945fc983b96
Won Yang <won@gatech.edu>
2021-05-25 16:49:39 -0500

    adds driver code

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,6 @@
+import random
+
+
 class GuessingGame():
     answer = 0
     is_solved = False
@@ -9,6 +12,8 @@ class GuessingGame():
         return self.is_solved
 
     def guess(self, user_guess):
+        if (type(user_guess) == str):
+            user_guess = int(user_guess)
         if (user_guess == self.answer):
             self.is_solved = True
             return 'correct'
@@ -18,13 +23,30 @@ class GuessingGame():
             return 'low'
 
 
-game = GuessingGame(10)
+# game = GuessingGame(10)
 
-print(game.solved())   # => False
+# print(game.solved())   # => False
 
-print(game.guess(5))  # => 'low'
-print(game.guess(20))  # => 'high'
-print(game.solved())   # => False
+# print(game.guess(5))  # => 'low'
+# print(game.guess(20))  # => 'high'
+# print(game.solved())   # => False
 
-print(game.guess(10))  # => 'correct'
-print(game.solved())   # => True
+# print(game.guess(10))  # => 'correct'
+# print(game.solved())   # => True
+
+
+# ----- DRIVER CODE -----
+game = GuessingGame(random.randint(1, 100))
+last_guess = None
+last_result = None
+
+while game.solved() == False:
+    if last_guess != None:
+        print(f"Oops! Your last guess ({last_guess}) was {last_result}.")
+        print("")
+
+    last_guess = input("Enter your guess: ")
+    last_result = game.guess(last_guess)
+
+
+print(f"{last_guess} was correct!")
